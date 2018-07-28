c9a7b72693ab6680a39188213b72901945f8a017
CherryLu <15551283679@163.com>
2018-07-28 16:18:36 +0800

    Item提交

--- app/src/main/java/com/chinamobile/wanapp/baen/BaseItem.java
+++ app/src/main/java/com/chinamobile/wanapp/baen/BaseItem.java
@@ -5,7 +5,13 @@ package com.chinamobile.wanapp.baen;
  */
 
 public class BaseItem {
-    private int type; //视图类型
+    public static int ITEM_LOGO_MEASSAGE = 0;//logo+消息按钮
+    public static int ITEM_ICON = 1;//logo+消息按钮
+    public static int ITEM_BANNER = 2;//banner
+    public static int ITEM_ROLL = 3;//rolltxt;
+    public static int ITEM_TAB_LIST = 4;//tab栏
+
+    private int type; //视图类型 0
 
     public int getType() {
         return type;
