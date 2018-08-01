e4cfdbc06d372f773ff41983ae731fa4551898d1
CherryLu <g15551283679@163.com>
2018-08-02 00:07:15 +0800

    增加详情页

---
+++ app/src/main/java/com/chinamobile/wanapp/ui/activity/TaskDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.chinamobile.wanapp.ui.activity;
+
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2018/8/1.
+ */
+
+public class TaskDetailsActivity extends BaseActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
